ea016478eae20d4d930d2bd49c94b4205e2ec2b6
Austin Winslow <awinslow21@gmail.com>
2014-01-03 12:54:52 -0500

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Work2transcend::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Work2transcend::Application.config.session_store :cookie_store, key: '_work2transcend_session'
