9930354c7f76bfc327bbf19ce16c40e9bc172085
Austin Winslow <awinslow21@gmail.com>
2014-01-09 12:06:03 -0500

    changed view/edit/delete permissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108154320) do
+ActiveRecord::Schema.define(version: 20140109163946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,22 +59,23 @@ ActiveRecord::Schema.define(version: 20140108154320) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
-    t.integer  "age",                                 null: false
-    t.string   "state",                               null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "first_name",                              null: false
+    t.string   "last_name",                               null: false
+    t.integer  "age",                                     null: false
+    t.string   "state",                                   null: false
+    t.string   "email",                  default: "",     null: false
+    t.string   "encrypted_password",     default: "",     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role",                   default: "user"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
---
+++ spec/features/admin_deletes_a_project_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+feature "Admin deletes a project" do
+  scenario 'Visitors cannot delete projects'
+  scenario 'Users cannot delete projects'  
+end
